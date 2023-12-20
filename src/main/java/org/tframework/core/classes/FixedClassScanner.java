1a74c42630569aef99fb9dde8b53a7e603682327
Gtomika <gaspar.tamas97@protonmail.com>
2023-12-20 14:44:09 +0100

    #13: Added 2 simple ClassScanner implementations.

---
+++ src/main/java/org/tframework/core/classes/FixedClassScanner.java
@@ -0,0 +1,25 @@
+/* Licensed under Apache-2.0 2023. */
+package org.tframework.core.classes;
+
+import java.util.List;
+
+/**
+ * A {@link ClassScanner} implementation that does not actually perform any scanning,
+ * but rather returns the classes provided at construction time.
+ */
+public class FixedClassScanner implements ClassScanner {
+
+    private List<Class<?>> classes;
+
+    /**
+     * Create a scanner that will return the provided classes on scan.
+     */
+    public FixedClassScanner(List<Class<?>> classes) {
+        this.classes = classes;
+    }
+
+    @Override
+    public List<Class<?>> scanClasses() {
+        return List.copyOf(classes);
+    }
+}

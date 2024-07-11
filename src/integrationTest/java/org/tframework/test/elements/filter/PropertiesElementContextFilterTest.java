ee6a24f7a1c9605c77ea7fd66d120785f96b5e3b
Gtomika <gaspar.tamas97@protonmail.com>
2024-07-11 10:29:04 +0200

    #101: Spotless apply.

--- src/integrationTest/java/org/tframework/test/elements/filter/PropertiesElementContextFilterTest.java
+++ src/integrationTest/java/org/tframework/test/elements/filter/PropertiesElementContextFilterTest.java
@@ -1,4 +1,22 @@
+/*
+Copyright 2024 Tamas Gaspar
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package org.tframework.test.elements.filter;
+
+import static org.tframework.test.commons.utils.TframeworkAssertions.assertHasElement;
+import static org.tframework.test.commons.utils.TframeworkAssertions.assertHasNoElement;
 
 import org.junit.jupiter.api.Test;
 import org.tframework.core.elements.ElementsContainer;
@@ -7,9 +25,6 @@ import org.tframework.core.elements.annotations.InjectElement;
 import org.tframework.core.elements.context.filter.annotation.RequiredProperty;
 import org.tframework.test.commons.annotations.SetProperties;
 import org.tframework.test.junit5.IsolatedTFrameworkTest;
-
-import static org.tframework.test.commons.utils.TframeworkAssertions.assertHasElement;
-import static org.tframework.test.commons.utils.TframeworkAssertions.assertHasNoElement;
 
 @IsolatedTFrameworkTest
 @SetProperties({"my.property=value", "my.other.property=otherValue"})
--- src/test/java/org/tframework/core/elements/context/filter/PropertyElementContextFilterTest.java
+++ src/test/java/org/tframework/core/elements/context/filter/PropertyElementContextFilterTest.java
@@ -1,5 +1,26 @@
+/*
+Copyright 2024 Tamas Gaspar
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package org.tframework.core.elements.context.filter;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+import java.lang.reflect.AnnotatedElement;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,13 +34,6 @@ import org.tframework.core.elements.context.source.ElementSource;
 import org.tframework.core.properties.PropertiesContainer;
 import org.tframework.core.properties.PropertyNotFoundException;
 import org.tframework.core.reflection.annotations.AnnotationScanner;
-
-import java.lang.reflect.AnnotatedElement;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class PropertyElementContextFilterTest {
